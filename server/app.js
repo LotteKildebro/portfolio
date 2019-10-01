af59e5d2861cb7d5d21221dd6fb5241b76daaf91
LotteKildebro <4514756@RTS-365.dk>
2019-10-01 21:39:56 +0200

    app port

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ directories.forEach(directory => {
 app.use(express.static('public'));
 
 // start serveren på port 3000
-const port = 3001;
+const port = 3002;
 app.listen(port, (err) => {
     if (err) {
         console.log(err);
